d58f204aaa86308fc73e857b612c004defbb9f7b
killi <killi@BEAST_MK2>
2026-09-21 11:49:26 +0100

    Add project files.

---
+++ Models/LoanRecord.cs
@@ -0,0 +1,8 @@
+﻿namespace TechVault.UI.Models;
+
+public class LoanRecord
+{
+    public int StudentId { get; set; }
+    public string EquipmentType { get; set; } = string.Empty;
+    public DateTime ExpectedReturn { get; set; } = DateTime.Now.AddDays(1);
+}
---
+++ Models/UserProfile.cs
@@ -0,0 +1,9 @@
+﻿namespace TechVault.UI.Models;
+
+public class UserProfile
+{
+    public string Username { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public DateTime DateOfBirth { get; set; } = DateTime.Now.AddYears(-16);
+    public string Role { get; set; } = string.Empty;
+}
